60a438cdf7f0903ce27983f58c55d976ead0c054
purin_private <mitsu20030816@gmail.com>
2024-10-11 17:43:31 +0900

    情報と著作権おわり！

--- 技大/B3_2学期/情報システム工学演習/Web開発/03_JavaScript/配布物/sample_01_font/sample_01.js
+++ 技大/B3_2学期/情報システム工学演習/Web開発/03_JavaScript/配布物/sample_01_font/sample_01.js
@@ -1,8 +1,11 @@
-
+function changeColor() {
+  document.getElementById('message').style.color = 'red';
+  document.getElementById('message').style.fontSize = '48px';
+}
 
 
 
 window.onload=function(){
-  
+  document.getElementById('change').onclick = changeColor;
 }
 
