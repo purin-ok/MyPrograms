40a6f73dc989b1adfcca72b143af9c3a4a046f2c
purin_private <mitsu20030816@gmail.com>
2024-10-14 09:52:12 +0900

    最低限は完成

--- 技大/B3_2学期/情報システム工学演習/Web開発/03_JavaScript/課題/24336488_Q2.js
+++ 技大/B3_2学期/情報システム工学演習/Web開発/03_JavaScript/課題/24336488_Q2.js
@@ -1,5 +1,7 @@
-
+changeNumber = function () {
+  document.getElementById("number-ans").innerHTML = this.value;
+}
 
 window.onload = function() {
-  document.getElementById("student-number").onkeyup
+  document.getElementById("student-number").onkeyup=changeNumber
 }
