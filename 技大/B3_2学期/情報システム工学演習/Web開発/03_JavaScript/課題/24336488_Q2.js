a2377eca55ec410da596b24ec15dd122d794ad0e
purin_private <mitsu20030816@gmail.com>
2024-10-14 11:41:13 +0900

    SPXまで

--- 技大/B3_2学期/情報システム工学演習/Web開発/03_JavaScript/課題/24336488_Q2.js
+++ 技大/B3_2学期/情報システム工学演習/Web開発/03_JavaScript/課題/24336488_Q2.js
@@ -3,5 +3,19 @@ changeNumber = function () {
 }
 
 window.onload = function() {
-  document.getElementById("student-number").onkeyup=changeNumber
+  document.getElementById("student-number").onkeyup = changeNumber
+  
+  // チェックボックスの処理
+  const checkboxes = document.getElementsByName("know");
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", updateCheckboxValues);
+  });
+  
+  // ドロップダウンリストの処理1
+  const dropdown_1 = document.getElementById("country-id");
+  dropdown_1.addEventListener("change", updateDropdown1Value);
+
+  // ドロップダウンリストの処理2
+  const dropdown_2 = document.getElementById("problem-select");
+  dropdown_2.addEventListener("change",updateDropdown2Value)
 }
